0308a5e7655ff98b1dac6585aa5ed15f00af17af
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-08-06 22:04:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,4 @@ def webhook():
         }), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Port codé en dur
