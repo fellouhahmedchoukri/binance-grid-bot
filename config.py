1543debad735de07b45b59156ff5d519fdfc69bb
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-08-06 21:35:51 +0100

    Update config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration Binance - REMPLACEZ PAR VOS CLÉS!
+API_KEY = "VOTRE_CLE_API_BINANCE"
+API_SECRET = "VOTRE_SECRET_API_BINANCE"
+
+# Passphrase pour sécuriser les webhooks
+WEBHOOK_PASSPHRASE = "#1960AlGeR@+="
+
+# Mode test (True pour tester sans risque)
+TEST_MODE = True
