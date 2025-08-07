34e87f05ae50c28896f2e19505ad5f59a7d37de4
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-08-07 18:22:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,3 +76,8 @@ def webhook():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+#reserve a render.com
+@app.route("/", methods=["GET"])
+def home():
+    return "Binance Grid Bot is running!", 200
